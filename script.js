6bbe5350ac6edd023923446d19abf9afb7dc6472
nikolaigerbers <nikolai.gerbers@googlemail.com>
2026-07-14 22:27:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1007,8 +1007,6 @@ document.querySelectorAll(".slider").forEach(slider => {
 
   let currentIndex = 0;
 
-  let fullscreenDirection = 1;
-
   function buildFullscreenDots(count) {  // ← neu
     if (!fullscreenDotsContainer) return;
     fullscreenDotsContainer.innerHTML = "";
